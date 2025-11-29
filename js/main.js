bae78c513a04cf6913dd3834b1db0722a2884dc4
jomiy191-arch <jomiy191@gmail.com>
2025-11-29 14:06:46 +0500

    Some changes

--- js/main.js
+++ js/main.js
@@ -183,3 +183,13 @@ function renderProducts() {
 }
 
 renderProducts();
+
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    const content = document.getElementById('content');
+
+    setTimeout(function() {
+        preloader.style.display = 'none';
+        content.style.display = 'block';
+    }, 3000); 
+});
